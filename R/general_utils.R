20d7cebe2c10bcd373456b2c5e2cc22d1324fbf8
Mazin Bokhari <mazin.bokhari@robinhood.com>
2019-09-24 15:48:16 -0700

    Update

--- R/general_utils.R
+++ R/general_utils.R
@@ -1,14 +1,15 @@
+#' @export
 pass = function(x=NULL){x}
 
-zplot = function(df_input) {df_input %>% read.zoo %>% autoplot(facets=NULL) %>% ggplotly}
-
+#' @export
 `%nin%` = Negate(`%in%`)
 
 #' This function allows you to collapse a list with the pipe separator (useful for select(matches()) subsetting)
 #' @param input_list the list to be collapsed
-#' pipe_collapse()
+#' @export
 pipe_collapse = function(input_list) {
     input_list %>% glue::glue_collapse('|')
 }
 
+#' @export
 matches_list = function(input) {tidyr::matches(input %>% glue::glue_collapse('|'))}
